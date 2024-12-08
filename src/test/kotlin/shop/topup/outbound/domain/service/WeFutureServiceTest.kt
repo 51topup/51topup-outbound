c87bbc4bd90eec7a89304c3f412efe1efe249fe3
Libing Chen <libing.chen@gmail.com>
2024-12-08 15:33:45 +0800

    test: WeFutureServiceTest

---
+++ src/test/kotlin/shop/topup/outbound/domain/service/WeFutureServiceTest.kt
@@ -0,0 +1,52 @@
+package shop.topup.outbound.domain.service
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
+import org.apache.commons.text.StringEscapeUtils
+import kotlin.test.Test
+
+class WeFutureServiceTest {
+    val weFutureService = WeFutureService(jacksonMapperBuilder().apply {
+        disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    }.build())
+    val sellerId = "35132"
+    val secret = "831f54f132b2542016fca10cfe7cec2b"
+
+    @Test
+    fun fetchBalance() {
+        val balance = weFutureService.fetchBalance(sellerId, secret)
+        println(balance)
+    }
+
+    @Test
+    fun fetchGroups() {
+        val groups = weFutureService.fetchGroups(sellerId, secret)
+        println("groupname,groupaliasname,groupid,groupimgurl,brandid,brandname,brandimgurl")
+        groups.forEach {
+            val groupname = StringEscapeUtils.escapeCsv(it.groupname)
+            val groupaliasname = StringEscapeUtils.escapeCsv(it.groupaliasname)
+            val brandname = StringEscapeUtils.escapeCsv(it.brandname)
+            println("${groupname},${groupaliasname},${it.groupid},${it.groupimgurl},${it.brandid},${brandname},${it.brandimgurl}")
+        }
+    }
+
+    @Test
+    fun testFetchItems() {
+        println("goodsid,imgurl,goodsname,goodsprice,goodsstatus,goodstype,stock,buyminnum,goodsgroupid")
+        (0..33).forEach { i ->
+            val result = weFutureService.fetchItems(sellerId, secret, i)
+            result.data.forEach {
+                val goodsname = StringEscapeUtils.escapeCsv(it.goodsname)
+                println("${it.goodsid},${it.imgurl},${goodsname},${it.goodsprice},${it.goodsstatus},${it.goodstype},${it.stock},${it.buyminnum},${it.goodsgroupid}")
+            }
+            Thread.sleep(10000)
+        }
+
+    }
+
+    @Test
+    fun fetchItemDetail() {
+        val item = weFutureService.fetchItemDetail(sellerId, secret, 413353)
+        println(item)
+    }
+}
