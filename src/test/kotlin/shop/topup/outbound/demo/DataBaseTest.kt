b3412797847e2b379f94bd2dc93a4ff327fa251d
Libing Chen <libing.chen@gmail.com>
2024-12-08 15:33:00 +0800

    test: database test

---
+++ src/test/kotlin/shop/topup/outbound/demo/DataBaseTest.kt
@@ -0,0 +1,25 @@
+package shop.topup.outbound.demo
+
+import com.github.database.rider.core.api.dataset.DataSet
+import org.dbunit.database.DatabaseConnection
+import org.dbunit.database.IDatabaseConnection
+import org.dbunit.dataset.xml.FlatDtdDataSet
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import shop.topup.workspace.TestcontainersBaseTest
+import java.io.FileOutputStream
+import javax.sql.DataSource
+import kotlin.jvm.Throws
+
+@DataSet("/db/dataset/suppliers.xml")
+class DataBaseTest : TestcontainersBaseTest() {
+    @Autowired
+    private lateinit var dataSource: DataSource
+
+    @Test
+    @Throws(Exception::class)
+    fun generateDTD() {
+        val connection: IDatabaseConnection = DatabaseConnection(dataSource.connection)
+        FlatDtdDataSet.write(connection.createDataSet(), FileOutputStream("database.dtd"))
+    }
+}
