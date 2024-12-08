07bd352cd370659f227cf65da6e897158e21b52e
Libing Chen <libing.chen@gmail.com>
2024-12-08 15:36:22 +0800

    chore: implement logic

--- src/main/kotlin/shop/topup/outbound/domain/service/WeFutureSyncService.kt
+++ src/main/kotlin/shop/topup/outbound/domain/service/WeFutureSyncService.kt
@@ -1,33 +1,34 @@
 package shop.topup.outbound.domain.service
 
-import org.jooq.DSLContext
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
 import shop.topup.admin.domain.common.jooq.tables.SupplierCatalog
 import shop.topup.admin.domain.common.jooq.tables.SupplierGoods
+import shop.topup.admin.domain.common.jooq.tables.records.SupplierCatalogRecord
 import shop.topup.outbound.domain.common.dao.SupplierCatalogDAO
+import shop.topup.outbound.domain.common.dao.SupplierDAO
 import shop.topup.outbound.domain.common.dao.SupplierGoodsDAO
 
 
 @Service
 class WeFutureSyncService(
-    val dslContext: DSLContext,
+    val supplierDAO: SupplierDAO,
     val supplierCatalogDAO: SupplierCatalogDAO,
     val supplierGoodsDAO: SupplierGoodsDAO,
     val weFutureService: WeFutureService
 ) {
-    val sellerId = "35132"
-    val secret = "831f54f132b2542016fca10cfe7cec2b"
     val log: Logger = org.slf4j.LoggerFactory.getLogger(WeFutureSyncService::class.java)
+    val supplierId: Long = 1
 
     fun syncGroups() {
-        val catalogs = weFutureService.fetchGroups(sellerId, secret)
+        val supplierApiInfo = supplierDAO.findApiInfo(supplierId)!!
+        val catalogs = weFutureService.fetchGroups(supplierApiInfo.apiKey, supplierApiInfo.apiSecret)
         log.error("Sync ${catalogs.size} catalogs from 1 ")
-        catalogs.subList(0, 3).forEach {
-            var catalog = supplierCatalogDAO.findBySupplier(1, it.groupid)
+        catalogs.forEach {
+            var catalog = supplierCatalogDAO.findBySupplier(supplierId, it.groupid)
             if (catalog == null) {
                 catalog = SupplierCatalog.SUPPLIER_CATALOG.newRecord().apply {
-                    this.supplierId = 1
+                    this.supplierId = supplierApiInfo.id
                     this.groupId = it.groupid
                 }
             }
@@ -39,16 +40,22 @@ class WeFutureSyncService(
     }
 
     fun syncGoods() {
+        val supplierApiInfo = supplierDAO.findApiInfo(supplierId)!!
+        val catalogs: Map<Long, SupplierCatalogRecord> =
+            supplierCatalogDAO.findAll(SupplierCatalog.SUPPLIER_CATALOG.SUPPLIER_ID.eq(supplierId)).associateBy { it.groupId }
         (0..33).forEach { i ->
             log.info("Sync goods: { page: $i }")
-            val result = weFutureService.fetchItems(sellerId, secret, i)
+            val result = weFutureService.fetchItems(supplierApiInfo.apiKey, supplierApiInfo.apiSecret, i)
             log.info("Sync goods: { page: $i, count: ${result.count} }")
             result.data.forEach {
-                var goods = supplierGoodsDAO.findBySupplier(1, it.goodsid)
+                var goods = supplierGoodsDAO.findBySupplier(supplierApiInfo.id, it.goodsid)
                 if (goods == null) {
                     goods = SupplierGoods.SUPPLIER_GOODS.newRecord().apply {
-                        this.supplierId = 1
+                        this.supplierId = supplierId
                         this.supplierGoodsId = it.goodsid
+                        this.supplierGroupId = it.goodsgroupid
+                        this.type = it.goodstype
+                        this.catalogId = catalogs[it.goodsgroupid]?.id ?: 0
                     }
                 }
                 goods.name = it.goodsname
@@ -60,6 +67,7 @@ class WeFutureSyncService(
                 supplierGoodsDAO.save(goods)
                 log.info("Sync goods: { id: ${goods.supplierGoodsId}, name:${goods.name} }")
             }
+            Thread.sleep(5000) // 5 seconds delay because 3 seconds delay required
         }
     }
 
