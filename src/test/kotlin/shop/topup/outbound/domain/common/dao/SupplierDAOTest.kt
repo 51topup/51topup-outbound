b8dffd8f4e8e47291aa086026dac2be384e850fb
Libing Chen <libing.chen@gmail.com>
2024-12-08 15:33:18 +0800

    test: SupplierDAOTest

---
+++ src/test/kotlin/shop/topup/outbound/domain/common/dao/SupplierDAOTest.kt
@@ -0,0 +1,18 @@
+package shop.topup.outbound.domain.common.dao
+
+import com.github.database.rider.core.api.dataset.DataSet
+import org.springframework.beans.factory.annotation.Autowired
+import shop.topup.workspace.TestcontainersBaseTest
+import kotlin.test.Test
+
+@DataSet("/db/dataset/suppliers.xml")
+class SupplierDAOTest : TestcontainersBaseTest() {
+    @Autowired
+    private lateinit var supplierDAO: SupplierDAO
+
+    @Test
+    fun testFindById() {
+        val supplier = supplierDAO.findById(1)
+        println(supplier)
+    }
+}
